6e01bed47507e6ad7ef76423386afd0e82adbbfc
mohamad mirani <mohammadmirani@gmail.com>
2021-02-18 18:38:02 +0330

    edited

--- Q6/routes/pages.js
+++ Q6/routes/pages.js
@@ -3,22 +3,10 @@ const pages =express.Router();
 const path = require('path')
 
 
+pages.get('/:i',(req, res, next)=>{
+    const {i} =req.params; 
+    res.sendFile(path.join(__dirname,`../public/index${i}.html`))
 
-pages.get('/page1',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../public/index1.html'))
-})
-pages.get('/page2',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../public/index2.html'))
-})
-pages.get('/page3',(req,res)=>{
-    res.sendFile(path.join(__dirname,"../public/index3.html"))
-})
-
-pages.get('/page4',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../public/index4.html'))
-})
-pages.get('/page5',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../public/index5.html'))
 })
 
 module.exports = pages;
